58f987ec4d52e913d3998292c42775872df9d417
Murodil <murodil01@gmail.com>
2025-07-02 17:05:02 +0500

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home"
-import About from "./pages/about"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
 import Projects from "./pages/projects";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
   );
 };
 
